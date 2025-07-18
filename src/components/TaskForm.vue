1730dfa9e2a692ade053f2c54d24e5fe5f1eebe4
mateuszmu <unnamed109@wp.pl>
2025-07-18 16:32:17 +0200

    walka w taskform z disabled

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,29 +1,40 @@
 <script setup>
-import {ref} from "vue";
+import {ref, watch} from "vue";
 
 const emit = defineEmits(['addTask'])
 const newTask = ref('')
 const info = ref('')
+
 
 function sendTask() {
   if (newTask.value.length > 0) {
     emit('addTask', newTask.value)
     newTask.value = ''
     info.value = ''
+
   } else {
     info.value = 'Wpisz treść zadania!'
+
+
   }
 }
+
 </script>
 
 <template>
   <div>
-    <input type="text" v-model="newTask" placeholder="Dodaj zadanie" @keyup.enter="sendTask"/>
-    <button @click="sendTask" >Dodaj</button>
+    <input type="text" v-model="newTask" placeholder="Dodaj zadanie" @keyup.enter="sendTask" autofocus/>
+    <button @click="sendTask" v-bind:disabled="newTask.length === 0">Dodaj</button>
   </div>
-  <label for='' >{{ info }}</label>
+  <label for=''>{{ info }}</label>
 </template>
 
 <style scoped>
+
+button:disabled:hover {
+  cursor: default;
+  pointer-events: none;
+
+}
 
 </style>
